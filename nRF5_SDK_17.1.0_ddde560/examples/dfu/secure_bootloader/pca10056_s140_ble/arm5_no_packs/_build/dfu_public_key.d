265a197a227e02bc165f01c796ee3455890a05e8
glory <1740301332@qq.com>
2025-04-29 19:58:06 +0800

    OTA

---
+++ nRF5_SDK_17.1.0_ddde560/examples/dfu/secure_bootloader/pca10056_s140_ble/arm5_no_packs/_build/dfu_public_key.d
@@ -0,0 +1,3 @@
+.\_build\dfu_public_key.o: ..\..\..\dfu_public_key.c
+.\_build\dfu_public_key.o: D:\keil\ARM\ARMCC\Bin\..\include\stdint.h
+.\_build\dfu_public_key.o: D:\keil\packs\NordicSemiconductor\nRF_DeviceFamilyPack\8.40.3\Device\Include\compiler_abstraction.h
